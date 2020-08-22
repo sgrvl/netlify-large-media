62c209ea14727c07945c96d782fb00e7350cc293
BuildTools <unconfigured@null.spigotmc.org>
2020-08-21 23:24:17 -0400

    testing with transformation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import test from "./img/IMG_0100.jpg?nf_resize=smartcrop&w=300&h=400";
 
 function App() {
-	return <div className="App">helloworld</div>;
+	return (
+		<>
+			<div className="App">helloworld</div>
+			<img src={test} alt="test" />
+		</>
+	);
 }
 
 export default App;
