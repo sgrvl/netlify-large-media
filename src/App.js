1c458c44a7de2d2e0349dd7256e8f4368b581366
BuildTools <unconfigured@null.spigotmc.org>
2020-08-21 23:55:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import test from "./img/IMG_0100.jpg?nf_resize=smartcrop&w=300&h=400";
+//import test from "./img/IMG_0100.jpg?nf_resize=smartcrop&w=300&h=400";
 
 function App() {
 	return (
 		<>
 			<div className="App">helloworld</div>
-			<img src={test} alt="test" />
+			<img src={process.env.PUBLIC_URL + "/img/IMG_0100.jpg"} alt="test" />
 		</>
 	);
 }
