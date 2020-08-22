6286ceb8240b45de95356d37749324a657f4c550
BuildTools <unconfigured@null.spigotmc.org>
2020-08-22 01:47:35 -0400

    img folder back in src

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,23 +2,20 @@ import React from "react";
 import { importAll } from "./utils";
 
 const Grid = () => {
-	const images = importAll(require.context("../../public/img", true, /jpg$/));
+	const images = importAll(require.context("../img", false, /jpg$/));
 
-	const regex = /IMG_\d{4}/g;
+	//const regex = /IMG_\d{4}/g;
 
 	return (
 		<div>
 			<p>this is a grid</p>
 			{Object.entries(images).map((img) => {
-				let found = img[1].match(regex);
+				//let found = img[1].match(regex);
 				return (
 					<img
 						style={{ width: "200px", height: "200px", objectFit: "cover" }}
 						key={`image ${img[0]}`}
-						src={
-							process.env.PUBLIC_URL +
-							`/img/${found}.jpg?nf_resize=smartcrop&w=200&h=200`
-						}
+						src={`${img[1]}?nf_resize=smartcrop&w=200&h=200`}
 						alt={img[0]}
 					/>
 				);
