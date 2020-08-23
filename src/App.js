0b43020904a4569ea1496eec56816690690801bb
BuildTools <unconfigured@null.spigotmc.org>
2020-08-23 18:22:38 -0400

    Grid + load time test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 import Grid from "./components/Grid";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+`;
 
 function App() {
 	return (
 		<>
-			<h1>Photo grid</h1>
 			<Grid />
+			<GlobalStyle />
 		</>
 	);
 }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Txt from "./list.txt";
+import styled from "styled-components";
 
 const Grid = () => {
 	const [images, setImages] = useState([]);
+
+	/* Text file from 'dir /b *.jpg > ../../src/components/list.txt' in img folder */
 
 	useEffect(() => {
 		fetch(Txt)
@@ -11,23 +14,47 @@ const Grid = () => {
 			.then(setImages);
 	}, []);
 
+	const StyledGrid = styled.div`
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+		gap: 1em;
+		margin: 1em;
+	`;
+
+	const ImageWrap = styled.div`
+		cursor: pointer;
+		overflow: hidden;
+		height: 0;
+		padding: 50% 0;
+		position: relative;
+		opacity: 0.9;
+	`;
+
+	const Image = styled.img`
+		height: 100%;
+		width: 100%;
+		object-fit: cover;
+		position: absolute;
+		top: 0;
+		left: 0;
+	`;
+
 	return (
-		<div>
-			<p>This is a grid</p>
-			<p>This is a test</p>
+		<StyledGrid>
 			{images.map((item, index) => {
 				return (
-					<img
-						key={`${item} ${index}`}
-						src={
-							process.env.PUBLIC_URL +
-							`/img/${item}?nf_resize=smartcrop&w=200&h=200`
-						}
-						alt={`${item}`}
-					/>
+					<ImageWrap key={`${item} ${index}`}>
+						<Image
+							src={
+								process.env.PUBLIC_URL +
+								`/img/${item}?nf_resize=smartcrop&w=200&h=200`
+							}
+							alt={`${item}`}
+						/>
+					</ImageWrap>
 				);
 			})}
-		</div>
+		</StyledGrid>
 	);
 };
 
