13360e1b58e8e43388faa197fe2643c730f8b447
BuildTools <unconfigured@null.spigotmc.org>
2020-08-29 14:54:12 -0400

    Update Grid.js

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,7 +6,7 @@ import LazyLoad from "react-lazyload";
 
 const StyledGrid = styled.div`
 	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+	grid-template-columns: repeat(auto-fill, minmax(275px, 1fr));
 	gap: 1em;
 	margin: 1em;
 `;
@@ -27,6 +27,8 @@ const ImageWrap = styled.div`
 const Image = styled.img`
 	height: 100%;
 	width: 100%;
+	max-width: 400px;
+	max-height: 400px;
 	object-fit: cover;
 	position: absolute;
 	top: 0;
